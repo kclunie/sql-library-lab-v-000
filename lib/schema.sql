e476e1c8dbd7431722f2b0e69de1a7538d161b41
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 19:43:14 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE tables (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT
+);
+
+CREATE TABLE cats (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT
+);
+
+CREATE TABLE cats (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT
+);
+
+CREATE TABLE cats (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT
+);
+
+CREATE TABLE cats (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT
+);
