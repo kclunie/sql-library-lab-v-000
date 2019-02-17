b854f822efc507326f7fbd750e7fa0f62129128a
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 21:13:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ FROM characters ORDER BY LENGTH(characters.motto) DESC LIMIT 1"
 end
 
 def select_value_and_count_of_most_prolific_species
-    "SELECT species FROM characters ORDER BY LENGTH(characters.motto) DESC LIMIT 1"
+    "SELECT species FROM characters GROUP BY species DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
