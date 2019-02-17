628b6fde0273115c1c03d95fd61a6d791f4071cd
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 21:40:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+    "SELECT books.title, books.year
+FROM books 
+INNER JOIN series 
+ON books.series_id = series.id WHERE series.id=1 ORDER BY(books.year)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
