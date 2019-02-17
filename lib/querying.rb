87477fd305c91163960dd59dada7330d9540e4d2
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 20:38:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year, series.id
-FROM owners 
-INNER JOIN cats_owners 
-ON owners.id = cats_owners.owner_id WHERE cats_owners.cat_id = 3;"
+FROM books 
+INNER JOIN series 
+ON books.id = series.id WHERE cats_owners.cat_id = 3;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
