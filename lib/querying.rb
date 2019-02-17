f2b8b614c49803060fabc96f3986fe567caa2897
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 20:50:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
     "SELECT character.name, character.motto
-FROM charcter ORDER BY(charcter.motto) LIMIT 1"
+FROM charcter ORDER BY(character.motto) LIMIT 1"
 end
 
 
